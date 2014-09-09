87f0789639896ed96fa2ef6daea3fd139030cd77
Max Lau <forevervmax@gmail.com>
2014-09-09 18:47:24 +0800

    updapte client

--- stream.go
+++ stream.go
@@ -1,22 +1,28 @@
 package room
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/funny/link"
+)
 
 type Stream struct {
+	link.Message
 	Content []byte
 }
 
 func NewStream(data interface{}) Stream {
+	var msg link.Message
 	jsonByte, _ := json.Marshal(data)
-	return Stream{jsonByte}
+	return Stream{msg, jsonByte}
 }
-
 func (msg Stream) RecommendPacketSize() uint {
 	return uint(len(msg.Content))
 }
 
-func (msg Stream) AppendToPacket(packet []byte) []byte {
-	return append(packet, msg.Content...)
+func (msg Stream) AppendToPacket(packet []byte) ([]byte, error) {
+	var err error
+	return append(packet, msg.Content...), err
 }
 
 type Box struct {
